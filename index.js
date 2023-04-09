1dfe2767c323880b2f306819e05114ef5b9ba9aa
Ayush Kumar Sinha <88913986+Ayushkr15@users.noreply.github.com>
2023-04-09 07:54:24 +0530

    improved landing page

--- index.js
+++ index.js
@@ -91,14 +91,15 @@ app.get("/", (req, res) => {
 
 <head>
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
 
 
 	<meta charset="UTF-8">
 	<title>RussAPI</title>
+
+
+<link rel="shortcut icon" type="image/ico" href="img/favicon.ico>
+
+
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"      
 		integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
